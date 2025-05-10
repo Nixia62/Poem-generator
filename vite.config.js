ce29d10fe5e53beebd6cc926f63f8f54b3a22f14
Nishxsm <nishantdandnaik156@gmail.com>
2025-05-10 18:29:45 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/labyrinth/', // 👈 Add this line!
+  base: '/Poem-generator/', // ✅ Match repo name exactly (case-sensitive)
 })
